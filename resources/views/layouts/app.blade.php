e9f84fd6eb5e66af24b89acbc51b3c5b73f66818
Abdulaziz Alzaabi <alzaabi@Abdulazizs-MacBook-Pro-2.local>
2020-10-25 13:27:26 +0400

    readme updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,13 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+
+                                <a class="dropdown-item" href="{{route('task.index')}}">
+
+                                    Tasks
+                                </a>
+
+                                <a class=" dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
